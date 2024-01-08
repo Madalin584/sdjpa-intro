7f1c2542c24596f3521a67bffa48448749f70c0a
Madalin <madalinmadalin223@gmail.com>
2024-01-08 22:03:57 +0200

    Added logs

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -5,7 +5,7 @@ import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
